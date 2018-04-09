0d036b5a1cd1f4e5145e586b240835e696e5523e
Hernan Grecco <hernan.grecco@gmail.com>
2018-04-09 01:34:43 -0300

    Added multiple examples

--- lantz_qt/__main__.py
+++ lantz_qt/__main__.py
@@ -13,7 +13,7 @@ def main(args=None):
     CHOICES[args.demo](pending)
 
 
-def test_panel_demo(args=None):
+def testpanel_demo(args=None):
     """Run simulators.
     """
 
@@ -28,15 +28,96 @@ def test_panel_demo(args=None):
     print('Please make sure that the simulator is running.\n'
           'You can start it by running the following command in another terminal:\n\n'
           'lantz sims fungen tcp')
-    from lantz_qt import start_test_app
+
     from lantz.drivers.examples import LantzSignalGenerator
-    from lantz_qt.objwrapper import wrap_driver_cls
+
+    from lantz_qt import start_test_app, wrap_driver_cls
+
     QLantzSignalGenerator = wrap_driver_cls(LantzSignalGenerator)
     with QLantzSignalGenerator('TCPIP::localhost::5678::SOCKET') as inst:
         start_test_app(inst)
 
 
-CHOICES = {'testpanel': test_panel_demo}
+def uifile_demo(args=None):
+    print('Please make sure that the simulator is running.\n'
+          'You can start it by running the following command in another terminal:\n\n'
+          'lantz sims fungen tcp')
+
+    import os
+
+    UIFILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets', 'fungen.ui')
+
+    from lantz.drivers.examples import LantzSignalGenerator
+
+    from lantz_qt import start_gui, wrap_driver_cls
+
+    QLantzSignalGenerator = wrap_driver_cls(LantzSignalGenerator)
+    with QLantzSignalGenerator('TCPIP::localhost::5678::SOCKET') as inst:
+        start_gui(UIFILE, inst)
+
+
+def featscan_demo(args=None):
+
+    print('Please make sure that the simulator is running.\n'
+          'You can start it by running the following command in another terminal:\n\n'
+          'lantz sims fungen tcp')
+
+    # We import a helper function to start the app
+    from lantz_qt import start_gui_app, wrap_driver_cls
+
+    # The block consists of two parts the backend and the frontend
+    from lantz_qt.blocks import FeatScan, FeatScanUi
+
+    # An this you know already
+    from lantz.drivers.examples import LantzSignalGenerator
+
+    QLantzSignalGenerator = wrap_driver_cls(LantzSignalGenerator)
+
+    with QLantzSignalGenerator('TCPIP::localhost::5678::SOCKET') as fungen:
+
+        # Here we instantiate the backend setting 'frequency' as the Feat to scan
+        # and specifying in which instrument
+        app = FeatScan('frequency', instrument=fungen)
+
+        # Now we use the helper to start the app.
+        # It takes a Backend instance and a FrontEnd class
+        start_gui_app(app, FeatScanUi)
+
+
+def flock_demo(args=None):
+
+    # TODO: Make this an actual flock
+
+    print('Please make sure that the simulator is running.\n'
+          'You can start it by running the following command in another terminal:\n\n'
+          'lantz sims fungen tcp')
+
+    # We import a helper function to start the app
+    from lantz_qt import start_gui_app, wrap_driver_cls
+
+    # The block consists of two parts the backend and the frontend
+    from lantz_qt.blocks import FeatScan, FeatScanUi
+
+    # An this you know already
+    from lantz.drivers.examples import LantzSignalGenerator
+
+    QLantzSignalGenerator = wrap_driver_cls(LantzSignalGenerator)
+
+    with QLantzSignalGenerator('TCPIP::localhost::5678::SOCKET') as fungen:
+
+        # Here we instantiate the backend setting 'frequency' as the Feat to scan
+        # and specifying in which instrument
+        app = FeatScan('frequency', instrument=fungen)
+
+        # Now we use the helper to start the app.
+        # It takes a Backend instance and a FrontEnd class
+        start_gui_app(app, FeatScanUi)
+
+
+
+CHOICES = {'testpanel': testpanel_demo,
+           'uifile': uifile_demo,
+           'featscan': featscan_demo}
 
 if __name__ == '__main__':
     main()

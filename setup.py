71c5b09451371cedb4e67f1def5c778b9c364411
Hernan <hernan.grecco@gmail.com>
2018-12-31 17:12:03 -0300

    Back to development: 0.5.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = '\n\n'.join([read('README'),
 __doc__ = long_description
 
 setup(name='lantz-qt',
-      version='0.5',
+      version='0.5.1.dev0',
       license='BSD',
       description='Instrumentation framework',
       long_description=long_description,
